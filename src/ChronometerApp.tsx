51acd286c3d28e8274f9b7dcf78c8d1d2af971b8
Hugo <hugo_mendoza_s@outlook.com>
2024-02-28 17:26:44 -0500

    feat: add Chakra UI library for user interface
    -Create dark mode theme

--- src/ChronometerApp.tsx
+++ src/ChronometerApp.tsx
@@ -1,6 +1,18 @@
+import { Button, useColorMode } from '@chakra-ui/react'
+
 const ChronometerApp = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+
   return (
-    <h1>Map</h1>
+    <>
+      <h1>Map</h1>
+      <Button
+        onClick={toggleColorMode}
+        colorScheme='teal'
+      >
+        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+      </Button>
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from './theme/theme.ts'
+
 import ChonometerApp from './ChronometerApp.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChonometerApp />
+    <ChakraProvider theme={theme}>
+      <ChonometerApp />
+    </ChakraProvider>
   </React.StrictMode>
 )
